e334cd232a2ef08e7bc11e13a30255368c702a04
Connor2308 <149149292+Connor2308@users.noreply.github.com>
2024-03-19 19:36:36 +0000

    Update index.php

--- Stage 3 Shared Project/index.php
+++ Stage 3 Shared Project/index.php
@@ -1,5 +1,5 @@
 <head>
-    <title>Javascrip: Working with Time</title>
+    <title>National Water Research Institute</title>
     <link rel="stylesheet" href="css/index.css"/>
     <link rel="stylesheet" href="css/map.css"/>
     <link rel="stylesheet" href="js/map.js"/>
