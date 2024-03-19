521b785668c3d27ddc74c2584b0e22d43b4455c3
Connor2308 <149149292+Connor2308@users.noreply.github.com>
2024-03-19 15:02:16 +0000

    Update hotels.php

---
+++ Stage 3 Shared Project/hotels.php
@@ -0,0 +1,48 @@
+<head>
+    <title>Javascrip: Working with Time</title>
+    <link rel="stylesheet" href="css/index.css"/>
+    
+</head>
+<body>
+    <div class="main">
+        <div class="icon">
+            <div class="logo">
+                <img src="/images/logo_jata_2022_en.png">
+            </div>
+        </div>
+        <div class="Menu">
+            <ul>
+                <li><a href="index.php">Home</a></li>
+            </ul>
+            <ul>
+                <li><a href="hotels.php">Hotels</a></li>
+            </ul>
+            <ul>
+                <li><a href="#"></a></li>
+            </ul>
+            <div class="Search">
+                <input class="srch" type="search" name="" placeholder="Search">
+                <a href="#"><button class="btn">Search</button></a>
+            </div>
+        </div>
+    </div>
+    <div class="content">
+        
+    </div>
+    <footer id="footer" class="footer-text-center">
+        <div class="panel">
+            <ul>
+                <li><a href="#">FAQs</a></li>
+            </ul>
+            <ul>
+                <li><a href="#">Privacy Policy</a></li>
+            </ul>
+            <ul>
+                <li><a href="#">Security Policy</a></li>
+            </ul>
+            <ul>
+                <li><a href="#">Site Map</a></li>
+            </ul>
+        </div>
+    </footer>
+</body>
