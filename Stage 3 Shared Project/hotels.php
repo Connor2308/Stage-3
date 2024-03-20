595281681c8f653c0fdfad818839af28699cdc3f
Connor2308 <149149292+Connor2308@users.noreply.github.com>
2024-03-20 13:21:55 +0000

    Update hotels.php

--- Stage 3 Shared Project/hotels.php
+++ Stage 3 Shared Project/hotels.php
@@ -1,7 +1,12 @@
 <head>
     <title>National Water Research Institute</title>
-    <link rel="stylesheet" href="css/index.css"/>
-    
+    <link rel="stylesheet" href="css/mobile.css" />
+    <link
+      rel="stylesheet"
+      href="css/index.css"
+      media="only screen and (min-width : 720px)"
+    />
+
 </head>
 <body>
     <div class="main">
