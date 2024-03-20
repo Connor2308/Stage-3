ac57ed4c261f0b0a8ec7ab6d200c2760800de253
Connor2308 <149149292+Connor2308@users.noreply.github.com>
2024-03-20 13:20:52 +0000

    Update index.php

--- Stage 3 Shared Project/index.php
+++ Stage 3 Shared Project/index.php
@@ -1,8 +1,14 @@
 <head>
     <title>National Water Research Institute</title>
-    <link rel="stylesheet" href="css/index.css"/>
     <link rel="stylesheet" href="css/map.css"/>
     <link rel="stylesheet" href="js/map.js"/>
+    <link rel="stylesheet" href="css/mobile.css" />
+    <link
+      rel="stylesheet"
+      href="css/index.css"
+      media="only screen and (min-width : 720px)"
+    />
+
 </head>
 <body>
     <div class="main">
