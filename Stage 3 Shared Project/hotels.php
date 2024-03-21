5a4f4deb1e85c3609eb805676948abcfd9942262
Felix <c3032522@hallam.shu.ac.uk>
2024-03-21 15:21:43 +0000

    Hotel page update

--- Stage 3 Shared Project/hotels.php
+++ Stage 3 Shared Project/hotels.php
@@ -10,44 +10,10 @@
 </head>
 <body>
     <div class="main">
-        <div class="icon">
-            <div class="logo">
-                <img src="images/logo_jata_2022_en.png">
-            </div>
-        </div>
-        <div class="Menu">
-            <ul>
-                <li><a href="index.php">Home</a></li>
-            </ul>
-            <ul>
-                <li><a href="hotels.php">Hotels</a></li>
-            </ul>
-            <ul>
-                <li><a href="#"></a></li>
-            </ul>
-            <div class="Search">
-                <input class="srch" type="search" name="" placeholder="Search">
-                <a href="#"><button class="btn">Search</button></a>
-            </div>
-        </div>
+        <?php include("includes/header.php")?>
     </div>
     <div class="content">
-        
+        <!-- content -->
     </div>
-    <footer id="footer" class="footer-text-center">
-        <div class="panel">
-            <ul>
-                <li><a href="#">FAQs</a></li>
-            </ul>
-            <ul>
-                <li><a href="#">Privacy Policy</a></li>
-            </ul>
-            <ul>
-                <li><a href="#">Security Policy</a></li>
-            </ul>
-            <ul>
-                <li><a href="#">Site Map</a></li>
-            </ul>
-        </div>
-    </footer>
+    <?php include("includes/footer.php")?>
 </body>
