d69e79928b2e201cd1b556b8b9f870e8f35d5f0c
Connor2308 <149149292+Connor2308@users.noreply.github.com>
2024-03-21 16:02:52 +0000

    Create About.php

---
+++ Stage 3 Shared Project/About.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>National Water Research Institute</title>
+    <link rel="stylesheet" href="css/mobile.css" />
+    <link
+      rel="stylesheet"
+      href="css/index.css"
+      media="only screen and (min-width : 720px)"
+    />
+
+</head>
+<body>
+    <div class="main">
+        <?php include("includes/header.php")?>
+        <div class="icon">
+            <div class="logo">
+                <img src="images/logo_jata_2022_en.png">
+            </div>
+        </div>
+       
+    </div>
+        <div class="content">
+          <h2>What is Lorem Ipsum?</h2>
+          <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, 
+            when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, 
+            but also the leap into electronic typesetting,<br> 
+            remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, 
+            and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
+        </div>
+        <?php include("includes/footer.php")?>
+</body>
+</html> 
