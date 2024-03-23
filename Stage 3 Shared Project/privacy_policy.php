3c2341f8ffc1858eaaf40be553b28742764a71ef
Connor2308 <149149292+Connor2308@users.noreply.github.com>
2024-03-23 16:38:10 +0000

    Create privacy_policy.php

---
+++ Stage 3 Shared Project/privacy_policy.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>National Water Research Institute</title>
+    <link rel="stylesheet" href="css/map.css"/>
+    <link rel="stylesheet" href="js/map.js"/>
+    <link rel="stylesheet" href="css/mobile.css" />
+    <link
+      rel="stylesheet"
+      href="css/index.css"
+      media="only screen and (min-width : 720px)"
+    />
+
+</head>
+<body>
+    <div class="main">
+        <?php include("includes/header.php")?>
+        <div class="icon">
+            <div class="logo">
+                <img src="images/logo_jata_2022_en.png">
+            </div>
+        </div>
+        
+    </div>
+        <div class="content">
+           
+        </div>
+        <?php include("includes/footer.php")?>
+</body>
+</html> 
