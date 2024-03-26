dcd6ff198de1aece49ec9cf278c27976a876d5ce
Felix <c3032522@hallam.shu.ac.uk>
2024-03-26 17:07:11 +0000

    hotels table

--- Stage 3 Shared Project/hotels.php
+++ Stage 3 Shared Project/hotels.php
@@ -1,4 +1,16 @@
+<?php
+require_once("config.php");
+//query to get all hotels
+$queryHotels = "SELECT * FROM hotelandhomestays";
+$resultHotels = $mysqli->query( $queryHotels );
+?>
+
+<!DOCTYPE html>
+<html lang="en">
 <head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
     <title>National Water Research Institute</title>
     <link rel="stylesheet" href="css/mobile.css" />
     <link
@@ -6,14 +18,30 @@
       href="css/index.css"
       media="only screen and (min-width : 720px)"
     />
-
 </head>
 <body>
+    <?php include("includes/header.php")?>
     <div class="main">
-        <?php include("includes/header.php")?>
-    </div>
-    <div class="content">
-        <!-- content -->
+        <!-- Hotel Listing Here -->
+        <div class="table-container">
+            <table>
+            <tr>
+            <th>Hotel</th>
+            <th>Hotel Address</th>
+            <th>Region</th>
+            </tr>
+            <?php
+            while ($obj = $resultHotels -> fetch_object()) {
+            echo "<tr>";
+            echo "<td>{$obj->hotel_name}</a></td>";
+            echo "<td>{$obj->street_address}</td>";
+            echo "<td>&pound; {$obj->region}</td>";
+            echo "</tr>";
+            }
+            ?>
+            </table>
+        </div>
     </div>
     <?php include("includes/footer.php")?>
 </body>
+</html>
