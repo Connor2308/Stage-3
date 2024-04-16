8dff78df8410465c8272ed986db782da2daf4835
Connor2308 <149149292+Connor2308@users.noreply.github.com>
2024-04-16 14:20:20 +0100

    Updated JavaScript Search function

--- Stage 3 Shared Project/js/map.js
+++ Stage 3 Shared Project/js/map.js
@@ -106,28 +106,70 @@ async function initMap() {
     markers.push(marker);//adding the marks to my array of displayed markers, this is for the clusting functionality
   });
   const MarkerClusterer = new markerClusterer.MarkerClusterer({ markers, map })
-}
 
-function initAutocomplete(){
+  // Create the search box and link it to the UI element.
   const input = document.getElementById("pac-input");
-  const SearchBox = new google.maps.places.SearchBox(input);
+  const searchBox = new google.maps.places.SearchBox(input);
 
-  map.controls[google.maps.controlPosition.Top_Left].push(input);
-
-  map.addEventListener("bounds_changed", () => {
-    SearchBox.setBounds(map.getBound());
+  map.controls[google.maps.ControlPosition.TOP_LEFT].push(input);
+  // Bias the SearchBox results towards current map's viewport.
+  map.addListener("bounds_changed", () => {
+    searchBox.setBounds(map.getBounds());
   });
 
-  let markers[];
-  SearchBox.addlistener("place_changed), => {  
-    const places = SearchBox.getPlaces();
-      if(places.length == 0){
+  let markers = [];
+
+  // Listen for the event fired when the user selects a prediction and retrieve
+  // more details for that place.
+  searchBox.addListener("places_changed", () => {
+    const places = searchBox.getPlaces();
+
+    if (places.length == 0) {
+      return;
+    }
+
+    // Clear out the old markers.
+    markers.forEach((marker) => {
+      marker.setMap(null);
+    });
+    markers = [];
+
+     // For each place, get the icon, name and location.
+    const bounds = new google.maps.LatLngBounds();
+
+    places.forEach((place) => {
+      if (!place.geometry || !place.geometry.location) {
+        console.log("Returned place contains no geometry");
         return;
       }
-  markers.forEach((marker) =>{
-    marker.setMap(null);
+
+      const icon = {
+        url: place.icon,
+        size: new google.maps.Size(71, 71),
+        origin: new google.maps.Point(0, 0),
+        anchor: new google.maps.Point(17, 34),
+        scaledSize: new google.maps.Size(25, 25),
+      };
+
+      // Create a marker for each place.
+      markers.push(
+        new google.maps.Marker({
+          map,
+          icon,
+          title: place.name,
+          position: place.geometry.location,
+        }),
+      );
+       if (place.geometry.viewport) {
+        // Only geocodes have viewport.
+        bounds.union(place.geometry.viewport);
+      } else {
+        bounds.extend(place.geometry.location);
+      }
+    });
+    map.fitBounds(bounds);
   });
-  markers = [];
-  }
+} 
+}
 }
 initMap();
